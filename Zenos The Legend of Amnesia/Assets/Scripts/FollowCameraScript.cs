3bfe9c125fe6248a4583d921d74d41e2847a0831
dimou <dimoutheodor2@gmail.com>
2020-08-20 19:27:23 +0300

    First Commit Zenox Legend of Amnesia

---
+++ Zenos The Legend of Amnesia/Assets/Scripts/FollowCameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraScript : MonoBehaviour
+{
+   
+    // Update is called once per frame
+          public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    void LateUpdate()
+    {
+        transform.position = target.position;
+        
+    }
+    
+}
---
+++ Zenos The Legend of Amnesia/Assets/Scripts/mainPlayerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainPlayerScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 20 ,0,Input.GetAxis("Vertical") * Time.deltaTime * 20);
+    }
+
+}
